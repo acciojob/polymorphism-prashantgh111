aa5e0b66a5dbc7269b73c09fc107959b69161c1e
prashantgh111 <107759833+prashantgh111@users.noreply.github.com>
2023-01-27 22:32:09 +0530

    Solution Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
-public class Main {
+import com.sun.jdi.connect.Connector;
 
+public class Main {
+    public static void main(String [] agrs) {
+
+        Product p = new Product();
+
+        // i just have to call it . not mention print anything in que;
+        p.product(10,5));
+        p.product(10,10,10));
+        p.product(5.0,10.0));
+
+    }
+
+    public static class Product{
+        public static int product(int x, int y) {
+            return x*y;
+        }
+        public static int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public static double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
